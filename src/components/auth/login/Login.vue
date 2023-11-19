2cd37d2ce92ee62f67e27047e213639d6983fbd5
VitorSSilva230 <vitorssilva230@gmail.com>
2023-11-18 22:29:00 -0300

    first commit

---
+++ src/components/auth/login/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container class="container-login">
+        <v-form class="form-login">
+            <div class="logo"> <img src="" alt="logo"> </div>
+            <div class="title"> <span>Seja bem-vindo </span> </div>
+            <div class="sub-title"> <span>Acesse sua conta e aproveite todos os cursos</span> </div>
+            <div class="form-data">
+                <div class="email">
+                    <v-text-field 
+                    label="Email"
+                    type="email"
+                    hint="Insira um email válido">
+                    </v-text-field>
+                </div>
+                <div class="password">
+                    <v-text-field 
+                    label="Senha"
+                    type="password"
+                    hint="Mínimo 8 caracteres">
+                    </v-text-field>
+                </div>
+            </div>
+            <div class="click-data">
+                <div class="connect"><span></span></div>
+                <div class="forgot-password"><span></span></div>
+            </div>
+            <div class="buttons">
+                <div class="login"><v-btn type="submit">Entrar</v-btn></div>
+                <div class="signup"><v-btn type="submit">Cadastrar-se</v-btn></div>
+            </div>
+            <div>
+                <div><span>Ou, acesse com:</span></div>
+                <div>
+                    <span class="another-login">Gooogle</span>
+                    <span class="another-login">GitHub</span>
+                    <span class="another-login">Facebook</span>
+                </div>
+            </div>
+        </v-form>
+        <div class="img-login"> <img src="../../img/fundo_login.png" alt=""> </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'LoginComponent',
+  data() {
+    return {
+      // Seus dados aqui
+    };
+  },
+  methods: {
+    // Seus métodos aqui
+  },
+  // Outras opções como computed, props, etc.
+};
+</script>
---
+++ src/components/views/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+<v-container class="container">
+        <v-form class="form-login">
+            <div class="logo"> <img src="" alt="logo"> </div>
+            <div class="title"> <span>Seja bem-vindo </span> </div>
+            <div class="sub-title"> <span>Acesse sua conta e aproveite todos os cursos</span> </div>
+            <div class="form-data">
+                <div class="email">
+                    <v-text-field 
+                    label="Email"
+                    type="email"
+                    hint="Insira um email válido">
+                    </v-text-field>
+                </div>
+                <div class="password">
+                    <v-text-field 
+                    label="Senha"
+                    type="password"
+                    hint="Mínimo 8 caracteres">
+                    </v-text-field>
+                </div>
+            </div>
+            <div class="click-data">
+                <div class="connect"><span></span></div>
+                <div class="forgot-password"><span></span></div>
+            </div>
+            <div class="buttons">
+                <div class="login"><v-btn type="submit">Entrar</v-btn></div>
+                <div class="signup"><v-btn type="submit">Cadastrar-se</v-btn></div>
+            </div>
+            <div>
+                <div><span>Ou, acesse com:</span></div>
+                <div>
+                    <span class="another-login">Gooogle</span>
+                    <span class="another-login">GitHub</span>
+                    <span class="another-login">Facebook</span>
+                </div>
+            </div>
+        </v-form>
+        <div class="img-login"> <img src="" alt=""> </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+/* Adicione estilos personalizados aqui, se desejar */
+</style>
